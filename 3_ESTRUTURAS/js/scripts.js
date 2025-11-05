829a1016dfd092c655600e1669eaf1bec20841c7
Gabrielbm48 <gabrielbm48@hotmail.com>
2025-11-05 17:55:23 -0300

    Salvando valores

---
+++ 3_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,18 @@
+// 1 - variáveis
+let nome = "Matheus";
+
+console.log(nome);
+
+nome = "Matheus Battisti";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32;
+
+console.log(typeof nome);
+
+console.log(typeof idade);
