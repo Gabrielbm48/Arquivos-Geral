487a67ea9910f25f9c7d3478db938430dc79782e
Gabrielbm48 <gabrielbm48@hotmail.com>
2025-11-06 17:52:10 -0300

    Uso do "for"

--- 3_ESTRUTURAS/js/scripts.js
+++ 3_ESTRUTURAS/js/scripts.js
@@ -16,3 +16,139 @@ console.log(idade);
 console.log(typeof nome);
 
 console.log(typeof idade);
+
+// 2 - mais sobre avariáveis
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10,
+  b = 20,
+  c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Matheus Battisti";
+
+const nomeCompleto = "João da Silva";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// // 3 - Prompt
+// const age = prompt("Digite a sua idade:")
+
+// console.log(`Você tem ${age} anos`)
+
+// // 4 - Alert
+
+// alert("Testando");
+
+// const z = 10;
+// alert(`número é ${z}`);
+
+// // 5 - Math
+// console.log(Math.max(5, 2, 1, 10));
+
+// console.log(Math.floor(5.14));
+
+// console.log(Math.ceil(5.14));
+
+// // 6 - Console
+// console.log("Teste");
+
+// console.error("erro!");
+
+// console.warn("aviso!");
+
+// // 7 - if
+// const m = 10;
+
+// if (m > 5) {
+//   console.log("M é maior que 5!");
+// }
+
+// const user = "João";
+
+// if (user === "João") {
+//   console.log("Olá João!");
+// }
+
+// if (user === "Maria") {
+//   console.log("Olá Maria");
+// }
+
+// console.log(user === "João", user === "Maria");
+
+// // 8 - else
+// const loggedIn = false;
+
+// if (loggedIn) {
+//   console.log("Está altenticado!");
+// } else {
+//   console.log("Não está autenticado!");
+// }
+
+// const q = 10;
+// const w = 15;
+
+// if (q > 5 && w > 20) {
+//   console.log("Números mais altos");
+// } else {
+//   console.log("Os números não são mais altos");
+// }
+
+// // 9 - else if
+// if (1 > 2) {
+//   console.log("Teste");
+// } else if (2 > 3) {
+//   console.log("Teste 2");
+// } else if (5 > 1) {
+//   console.log("Agora sim");
+// }
+
+// const userName = "Matheus";
+// const userAge = 31;
+
+// if (userName === "José") {
+//   console.log("Bem vindo José!");
+// } else if (userName === "Matheus" && userAge === 31) {
+//   console.log("Olá Matheus, você tem 31 anos!");
+// }
+
+// // 10 while
+
+// let p = 0;
+
+// while (p < 5) {
+//   console.log(`Repetindo ${p}`);
+//   p = p + 1;
+// }
+
+// // loop infinito (desativei para não ter problemas)
+
+// //11 - do while
+
+// let o = 10;
+
+// do {
+//   console.log(`valor de o: ${o}`);
+//   o--;
+// } while (o > 1);
+
+// 12 - for
+for (let t = 0; t < 10; t++) {
+  console.log("Repetindo algo");
+}
+
+let r = 10;
+
+for (r; r > 0; r = r - 1) {
+  console.log(`O r está diminuíndo: ${r}`);
+}
